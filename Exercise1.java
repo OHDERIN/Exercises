14a41f1910d8283d99a158c120b2047628e04a69
OHDERIN <76110829+OHDERIN@users.noreply.github.com>
2022-08-06 13:57:15 -0400

    Exercise 1

---
+++ Exercise1.java
@@ -0,0 +1,17 @@
+//Author: Derin
+//Simple program that prints a greeting after recieving user's input
+
+import java.util.Scanner;
+
+public class Exercise1 {
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Input your first name: ");
+        String fname = input.next();
+        System.out.print("Input your last name: ");
+        String lname = input.next();
+        System.out.println();
+        System.out.println("Hello \n"+fname+" "+lname);
+    }
+}
